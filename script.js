bf94e226467017585b573f381b6a2ec41fd9618c
Dana Hazen <danahazen79@gmail.com>
2015-09-21 01:34:20 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function thisIsConfusing(x) {
+	return x + 7;	
+}
+function dontUnderstand(y) {
+    return y * y;
+}
+function whatIsThis(z) {
+	return z + z +z;
+}
+console.log(thisIsConfusing(dontUnderstand(whatIsThis(2))));
